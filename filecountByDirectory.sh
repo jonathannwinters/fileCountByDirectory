9d1b3bd5b2a79d8ce42676b5493299ce2ef90c51
jonathannwinters <jonathan@winters.im>
2019-03-13 22:10:32 -0400

    Add files via upload

---
+++ filecountByDirectory.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#!!!!!!!!!!!!!!!!!!!!!! WORK IN PROGRESS DO NOT USE !!!!!!!!!!!!!!!!!!!!!!!
+
+############################################################################
+# Title: fileCountByDirectory.sh                                                   
+#                                                                          
+# Description: A linux shell script that recursively goes through every    
+# directory and finds the file+dir count. Then sorts descending by count.                                    
+#                                                               
+# Author: Jonathan N. Winters, jnw25@cornell.edu                           
+# Date:   March 13, 2019
+# Version: 0.1
+#
+############################################################################
+
+#backup current IFS variable
+OIFS=$IFS;
+IFS=",";
+
+#create CSV string of all directories starting in current directory
+dirs=$(find . -maxdepth 2 -type d | while read dir; do echo $dir"," | cut -c 3- | tr -d '\r' | tr -d '\n';  done) 
+
+#convert this string into an array
+dirArray=($dirs);
+declare -A countArray
+
+#loop through the array and get the file+dir counts in each directory
+for ((i=0; i<${#dirArray[@]}; ++i));  do    
+        dirName="${dirArray[$i]}"; 
+        count=`(ls ${dirArray[$i]}  | wc -l) | awk '{$1=$1};1'`;
+        #countArray[$dirName]=$count
+
+done
+
+
+
+#sort -n -t: -k 1         | sort -n -t: -k 1
+
+
+
+
+#reset the IFS variable back to before running this script
+IFS=$OIFS;
+
+
